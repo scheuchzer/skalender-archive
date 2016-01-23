be3ac07596a6828649cc9c4a86858e6a6e115f9e
Thomas Scheuchzer <thomas.scheuchzer@gmx.net>
2016-01-23 22:42:42 +0100

    chrome fix

--- app/app.js
+++ app/app.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Date.prototype.toLocaleFormat = Date.prototype.toLocaleFormat || function (pattern) {
+        return pattern.replace(/%Y/g, this.getFullYear()).replace(/%m/g, (this.getMonth() + 1)).replace(/%d/g, this.getDate());
+    };
+
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
     'ngRoute',
